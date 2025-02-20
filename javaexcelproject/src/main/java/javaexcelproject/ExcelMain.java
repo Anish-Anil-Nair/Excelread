a2247fafe88ece35117c2fa3916e56d1967daad4
Anish-Anil-Nair <anishanil40@gmail.com>
2025-02-20 21:29:13 +0530

    Comments added

--- javaexcelproject/src/main/java/javaexcelproject/ExcelMain.java
+++ javaexcelproject/src/main/java/javaexcelproject/ExcelMain.java
@@ -6,7 +6,7 @@ public class ExcelMain{
 public static void main(String[] args)throws IOException {
 String s=Excelread.readStringData(1,1);
 System.out.println(s);
-String s1=Excelread.readIntegerData(1,0);
+String s1=Excelread.readIntegerData(1,0); // Value passed
 System.out.println(s1);
 }
 }
